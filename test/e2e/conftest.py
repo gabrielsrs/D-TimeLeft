941315b4f1dde9b78a67bbf25eef95cb7a2117ae
Gabriel Reis <55472396+gabrielsrs@users.noreply.github.com>
2026-02-05 22:30:50 -0300

    test(fixture): create date

--- test/e2e/conftest.py
+++ test/e2e/conftest.py
@@ -1,1 +1,13 @@
-from ..fixtures.db import client
+import pytest
+from dataclasses import asdict
+
+from ..fixtures.db import client, collection
+from ..fixtures.data_factories.date_db_factory import DateDbFactory
+
+
+@pytest.fixture
+def date(collection):
+    date_object = DateDbFactory()
+    data = collection.insert_one(asdict(date_object))
+
+    return data
