295ca32f58bcdf3f29b6c6e1748e5b469d647b62
Gabriel Reis <55472396+gabrielsrs@users.noreply.github.com>
2026-05-19 22:50:06 -0300

    refactor: adapt handle and db to tests

--- src/config/db_connection.py
+++ src/config/db_connection.py
@@ -1,10 +1,20 @@
 from pymongo import MongoClient
-from flask import current_app
+from flask import current_app, has_app_context
+
+from dotenv import load_dotenv
+import os
 
 def create_conn():
     """Create database connection"""
-    client = MongoClient(current_app.config["MONGO_URI"])
-    db = client[current_app.config["MONGO_DB"]]
+
+    if has_app_context():
+        client = MongoClient(current_app.config["MONGO_URI"])
+        db = client[current_app.config["MONGO_DB"]]
+    else:
+        load_dotenv()
+        client = MongoClient(os.getenv("TEST_MONGO_URI"))
+        db = client[(os.getenv("TEST_DB_NAME"))]
+
     collection = db.dates
 
     return collection
